166c4ca5970012be131caeaafd6a9f9439aadd02
yocn <zhaoyingkun@163.com>
2019-08-09 17:36:43 +0800

    使用libyuv

--- libyuv/src/main/jni/Android.mk
+++ libyuv/src/main/jni/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+GLOBAL_C_INCLUDES :=$(LOCAL_PATH)/libyuv/include
+
 include $(CLEAR_VARS)
 
 #include $(LOCAL_PATH)/jni/Android.mk
@@ -7,6 +9,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := YUVTrans
 LOCAL_SRC_FILES := yuvInterface/YUVTrans.cpp
 
+LOCAL_C_INCLUDES := $(GLOBAL_C_INCLUDES)
 include $(BUILD_SHARED_LIBRARY)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
