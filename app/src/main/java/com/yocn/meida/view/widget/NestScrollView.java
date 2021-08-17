a00c7797a07936442ced7c7c033f3d583117449d
yocn <zhaoyingkun@163.com>
2021-08-17 15:12:12 +0800

    优化点击

--- app/src/main/java/com/yocn/meida/view/widget/NestScrollView.java
+++ app/src/main/java/com/yocn/meida/view/widget/NestScrollView.java
@@ -1,18 +1,15 @@
 package com.yocn.meida.view.widget;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ScrollView;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 
 import com.yocn.meida.util.LogUtil;
+
+import androidx.annotation.Nullable;
 
 /**
  * @Author yocn
@@ -20,10 +17,10 @@ import com.yocn.meida.util.LogUtil;
  * @ClassName NestScrollView
  */
 public class NestScrollView extends ScrollView {
-
-    private int mWidth, mHeight;
-    private int mChildHeight;
-    private boolean mHandle;
+    private static final int distance = 2;
+    private PointF downPoint = new PointF();
+    // 中间MOVE移动是否超过了distance
+    private boolean beyond = false;
 
     public NestScrollView(Context context) {
         super(context);
@@ -38,71 +35,67 @@ public class NestScrollView extends ScrollView {
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        mWidth = MeasureSpec.getSize(widthMeasureSpec);
-        mHeight = MeasureSpec.getSize(heightMeasureSpec);
-//        LogUtil.d("mWidth:" + mWidth + "   " + mHeight + "    " + this);
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        View child = getChildAt(0);
-        if (child != null) {
-            mChildHeight = child.getMeasuredHeight();
-//            LogUtil.d("childHeight:" + mChildHeight);
-        }
-
-        if (mChildHeight > mWidth / 2) {
-            mHandle = true;
-            //不知道为什么layout_gravity = "center"的时候Child会出现偏移，所以当需要处理的时候手动将layout_gravity去掉
-            FrameLayout.LayoutParams lp = (LayoutParams) child.getLayoutParams();
-            lp.gravity = LayoutParams.UNSPECIFIED_GRAVITY;
-            child.setLayoutParams(lp);
-        } else {
-            mHandle = false;
-        }
-
-        super.onLayout(changed, l, t, r, b);
-    }
-
-    boolean move = false;
-
-    @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
-        LogUtil.d("dispatchTouchEvent1111:" + event.toString());
-        if (mHandle) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    move = false;
-                    getParent().requestDisallowInterceptTouchEvent(true);
-                    LogUtil.d("dispatchTouchEvent:" + event.toString());
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    move = true;
-                    break;
-                case MotionEvent.ACTION_UP:
-                    getParent().requestDisallowInterceptTouchEvent(false);
-                    LogUtil.d("dispatchTouchEvent:" + event.toString());
-                    if (!move) {
-                        boolean call = callOnClick();
-                        LogUtil.d("callOnClick:" + call);
-                        if (!call) {
-                            View parent = (View) getParent();
-                            parent.callOnClick();
-                        }
+        LogUtil.d("dispatchTouchEvent:" + printEvent(event));
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downPoint = new PointF(event.getX(), event.getY());
+                getParent().requestDisallowInterceptTouchEvent(true);
+                LogUtil.d("dispatchTouchEvent:" + event.toString());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!beyond) {
+                    PointF movePoint = new PointF(event.getX(), event.getY());
+                    // beyond是否曾经超过过distance
+                    beyond = check(downPoint, movePoint, false);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                PointF upPoint = new PointF(event.getX(), event.getY());
+                getParent().requestDisallowInterceptTouchEvent(false);
+                LogUtil.d("dispatchTouchEvent2:" + printEvent(event));
+                LogUtil.d("dispatchTouchEvent2:" + beyond + " 2：" + check(downPoint, upPoint, true));
+                if (!beyond || check(downPoint, upPoint, true)) {
+                    boolean call = callOnClick();
+                    LogUtil.d("callOnClick:" + call);
+                    if (!call) {
+                        View parent = (View) getParent();
+                        parent.callOnClick();
                     }
-                    break;
-                default:
-            }
+                }
+                break;
+            default:
         }
         return super.dispatchTouchEvent(event);
     }
 
+    /**
+     * 检查两点之间的距离
+     *
+     * @param p1   点1
+     * @param p2   点2
+     * @param isIn 是在距离内还是外
+     * @return isIn = true: 检查两个点的距离是不是小于distance; isIn = false: 检查两个点的距离是不是大于distance
+     */
+    private boolean check(PointF p1, PointF p2, boolean isIn) {
+        double dis = Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p1.y), 2));
+        LogUtil.d("dist:" + dis + "   down:[" + p1.x + "," + p1.y + "]   up:[" + p2.x + "," + p2.y + "]");
+        return isIn ? dis < distance : dis > distance;
+    }
+
+    private String printEvent(MotionEvent event) {
+        StringBuilder sb = new StringBuilder();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                sb.append("ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                sb.append("ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                sb.append("ACTION_UP");
+                break;
+        }
+        return sb.toString();
+    }
 }
