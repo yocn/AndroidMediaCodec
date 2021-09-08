24758bd52c02bd5014f2fd059bf28c7ea4a87b3d
yocn <zhaoyingkun@163.com>
2021-09-08 16:47:37 +0800

    增加预览尺寸选择

--- app/src/main/java/com/yocn/meida/view/activity/camera/PreviewAllActivity.java
+++ app/src/main/java/com/yocn/meida/view/activity/camera/PreviewAllActivity.java
@@ -1,19 +1,15 @@
 package com.yocn.meida.view.activity.camera;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Size;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.yocn.media.R;
 import com.yocn.meida.camera.Camera2AllSizeProvider;
-import com.yocn.meida.util.CameraUtil;
 import com.yocn.meida.view.widget.AspectTextureView;
 import com.yocn.meida.view.widget.FlowLayout;
 
@@ -28,8 +24,7 @@ public class PreviewAllActivity extends BaseCameraActivity {
     private int index;
     private FlowLayout flowLayout;
     private TextView sizeTv;
-    private FrameLayout parentFl1;
-    private FrameLayout parentFl2;
+    private Button showBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,40 +43,17 @@ public class PreviewAllActivity extends BaseCameraActivity {
         mPreviewView2 = root.findViewById(R.id.tv_camera_2);
         flowLayout = findViewById(R.id.flow);
         sizeTv = findViewById(R.id.tv_size);
-        parentFl1 = findViewById(R.id.fl_camera_1);
-        parentFl2 = findViewById(R.id.fl_camera_2);
 
         Button changeBtn = findViewById(R.id.btn_change);
-        Button showBtn = findViewById(R.id.btn_show);
+        showBtn = findViewById(R.id.btn_show);
         changeBtn.setOnClickListener(clickListener);
         showBtn.setOnClickListener(clickListener);
     }
 
-    private void addTvs(FlowLayout flowLayout) {
-        if (flowLayout == null) {
-            return;
-        }
-        //往容器内添加TextView数据
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(10, 5, 10, 5);
-        flowLayout.removeAllViews();
-        for (int i = 0; i < outputSizes.size(); i++) {
-            TextView tv = new TextView(this);
-            tv.setPadding(28, 10, 28, 10);
-            tv.setText(outputSizes.get(i).toString());
-            tv.setMaxEms(10);
-            tv.setTextColor(getResources().getColor(R.color.write));
-            tv.setSingleLine();
-            tv.setBackgroundResource(R.color.h_half);
-            tv.setLayoutParams(layoutParams);
-            tv.setTag(R.id.size, outputSizes.get(i));
-            tv.setOnClickListener(clickListener);
-            flowLayout.addView(tv, layoutParams);
-        }
-    }
-
     @Override
     protected void initData() {
+        mPreviewView.setScaleType(AspectTextureView.ScaleType.FIT_CENTER);
+        mPreviewView2.setScaleType(AspectTextureView.ScaleType.CENTER_CROP);
         mCamera2Provider = new Camera2AllSizeProvider(this);
         mCamera2Provider.setGetCameraInfoListener(outputSizes -> {
             PreviewAllActivity.this.outputSizes = outputSizes;
@@ -106,6 +78,7 @@ public class PreviewAllActivity extends BaseCameraActivity {
                 isFlowShow = !isFlowShow;
                 boolean visible = flowLayout.getVisibility() == View.VISIBLE;
                 flowLayout.setVisibility(visible ? View.GONE : View.VISIBLE);
+                showBtn.setText(getResources().getString(visible ? R.string.show_all_size : R.string.hide_all_size));
             } else {
                 Object tag = v.getTag(R.id.size);
                 if (tag != null) {
@@ -121,5 +94,28 @@ public class PreviewAllActivity extends BaseCameraActivity {
     protected void onDestroy() {
         mCamera2Provider.closeCamera();
         super.onDestroy();
+    }
+
+    private void addTvs(FlowLayout flowLayout) {
+        if (flowLayout == null) {
+            return;
+        }
+        //往容器内添加TextView数据
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(10, 5, 10, 5);
+        flowLayout.removeAllViews();
+        for (int i = 0; i < outputSizes.size(); i++) {
+            TextView tv = new TextView(this);
+            tv.setPadding(28, 10, 28, 10);
+            tv.setText(outputSizes.get(i).toString());
+            tv.setMaxEms(10);
+            tv.setTextColor(getResources().getColor(R.color.write));
+            tv.setSingleLine();
+            tv.setBackgroundResource(R.color.h_half);
+            tv.setLayoutParams(layoutParams);
+            tv.setTag(R.id.size, outputSizes.get(i));
+            tv.setOnClickListener(clickListener);
+            flowLayout.addView(tv, layoutParams);
+        }
     }
 }
--- app/src/main/java/com/yocn/meida/view/widget/AspectTextureView.java
+++ app/src/main/java/com/yocn/meida/view/widget/AspectTextureView.java
@@ -6,15 +6,33 @@ import android.view.TextureView;
 
 import com.yocn.meida.util.LogUtil;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.IntDef;
+
 /**
  * @Author yocn
  * @Date 2019/8/13 6:48 PM
  * @ClassName MyTextureView
  */
 public class AspectTextureView extends TextureView {
+    @ScaleType
+    private int scaleType = ScaleType.FIT_CENTER;
+    private int previewW = 0;
+    private int previewH = 0;
 
-    private int ratioW = 0;
-    private int ratioH = 0;
+    @IntDef({
+            ScaleType.FIT_XY,
+            ScaleType.FIT_CENTER,
+            ScaleType.CENTER_CROP
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ScaleType {
+        int FIT_XY = 0;
+        int FIT_CENTER = 1;
+        int CENTER_CROP = 2;
+    }
 
     public AspectTextureView(Context context) {
         super(context);
@@ -28,19 +46,17 @@ public class AspectTextureView extends TextureView {
         super(context, attrs, defStyleAttr);
     }
 
-    /**
-     * 设置宽高比
-     *
-     * @param width
-     * @param height
-     */
+    public void setScaleType(@ScaleType int scaleType) {
+        this.scaleType = scaleType;
+    }
+
     public void setAspect(int width, int height) {
         if (width < 0 || height < 0) {
             throw new IllegalArgumentException("width or height can not be negative.");
         }
         post(() -> {
-            ratioW = width;
-            ratioH = height;
+            previewW = width;
+            previewH = height;
             //请求重新布局
             requestLayout();
         });
@@ -52,25 +68,39 @@ public class AspectTextureView extends TextureView {
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (0 == ratioW || 0 == ratioH) {
+        if (0 == previewW || 0 == previewH) {
             //未设定宽高比，使用预览窗口默认宽高
             setMeasuredDimension(width, height);
         } else {
-            int tarW, tarH;
-            //设定宽高比，调整预览窗口大小（调整后窗口大小不超过默认值）
-            if (width < height * ratioW / ratioH) {
+            int tarW = 0, tarH = 0;
+            if (scaleType == ScaleType.FIT_XY) {
                 tarW = width;
-                tarH = width * ratioH / ratioW;
-            } else {
-                tarW = height * ratioW / ratioH;
                 tarH = height;
+            } else if (scaleType == ScaleType.FIT_CENTER) {
+                //设定宽高比，调整预览窗口大小（调整后窗口大小不超过默认值）
+                if (width * 1F / height < previewW * 1F / previewH) {
+                    tarW = width;
+                    tarH = width * previewH / previewW;
+                } else {
+                    tarW = height * previewW / previewH;
+                    tarH = height;
+                }
+            } else if (scaleType == ScaleType.CENTER_CROP) {
+                if (width * 1F / height < previewW * 1F / previewH) {
+                    //屏幕比预览细长，不管缩小还是拉伸，都应该先满足height
+                    tarH = height;
+                    tarW = previewW * height / previewH;
+                } else {
+                    //预览比屏幕细长
+                    tarW = width;
+                    tarH = previewH * width / previewW;
+                }
             }
             LogUtil.d("AspectTextureView", "原来的:" + width + "|" + height
-                    + "  raw:" + ratioW + "|" + ratioH + "  后：" + tarW + ":" + tarH);
+                    + "  raw:" + previewW + "|" + previewH + "  后：" + tarW + ":" + tarH);
             setMeasuredDimension(tarW, tarH);
-
-            setTranslationX((width - tarW) * 1.0f / 2);
-            setTranslationY((height - tarH) * 1.0f / 2);
+            setTranslationX((width - tarW) * 1F / 2);
+            setTranslationY((height - tarH) * 1F / 2);
         }
 
     }
